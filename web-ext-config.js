e412a4c278c8896ce4bed122607f51c3b47dc0e5
Andreas Kössler <github@akoessler.com>
2022-07-30 17:44:22 +0200

    Ignore ps1 and svg files in web-ext

--- web-ext-config.js
+++ web-ext-config.js
@@ -2,6 +2,10 @@ module.exports = {
     // Global options:
     verbose: true,
     sourceDir: "v2",
+    ignoreFiles: [
+        "**/*.ps1",
+        "**/*.svg"
+    ],
     // Command options:
     build: {
         overwriteDest: true
