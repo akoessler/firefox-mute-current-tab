7388638899f35bdc8f14c7ede933c3d827efca99
Andreas Kössler <github@akoessler.com>
2024-06-21 23:14:55 +0200

    Style fixes

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    // Global options:
-    verbose: true,
-    sourceDir: "v2",
-    ignoreFiles: [
-        "**/*.ps1",
-        "**/*.svg"
-    ],
-    // Command options:
-    build: {
-        overwriteDest: true
-    }
+  // Global options:
+  verbose: true,
+  sourceDir: "v2",
+  ignoreFiles: [
+    "**/*.ps1",
+    "**/*.svg"
+  ],
+  // Command options:
+  build: {
+    overwriteDest: true
+  }
 };
